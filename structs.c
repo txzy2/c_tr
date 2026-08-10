5fbf3a9100197dfc3ae25d263dbd422fef373787
Kamaev Anton <alk@m2lab.ru>
2026-08-10 14:21:43 +0300

    fix struct

--- structs.c
+++ structs.c
@@ -9,6 +9,38 @@ struct User {
 	int8_t age;
 };
 
+typedef struct {
+	struct User **u;
+	size_t size;
+	size_t capacity;
+} UserVector;
+
+void vector_init(UserVector *uv, size_t capacity) {
+	uv->u = malloc(capacity * sizeof(struct User *));
+	uv->size = 0;
+	uv->capacity = capacity;
+}
+
+void push_back(UserVector *uv, struct User *u) {
+	if (uv->size == uv->capacity) {
+		uv->capacity *= 2;
+		uv->u = realloc(uv->u, uv->capacity * sizeof(struct User *));
+	}
+
+	uv->u[uv->size] = u;
+	uv->size++;
+}
+
+void free_vec(UserVector *uv) {
+	for (size_t i = 0; i < uv->size; i++) {
+		free(uv->u[i]);
+	}
+	free(uv->u);
+	uv->u = NULL;
+	uv->size = 0;
+	uv->capacity = 0;
+}
+
 struct User *createUser(const char *name, int age) {
 	if (name == NULL || name[0] == '\0' || strlen(name) >= 30 || age <= 0 || age > INT8_MAX) {
 		return NULL;
@@ -36,20 +68,26 @@ bool updateName(struct User *u, const char *name) {
 }
 
 int main() {
+	UserVector uv;
+	vector_init(&uv, 1);
 
 	struct User *u = createUser("Anton", 25);
+
 	if (u) {
-		printf("USER: %s (%d years)\n", u->name, u->age);
-		if (updateName(u, "Dima")) {
-			printf("AFTER EDIT: %s (%d years)\n", u->name, u->age);
-			free(u);
+		push_back(&uv, u);
+
+		if (!updateName(uv.u[0], "Dima")) {
+			free_vec(&uv);
+			printf("ERROR EDIT USER\n");
 			return 0;
 		}
-
-		printf("ERROR EDIT USER\n");
-		return 0;
 	}
 
-	free(u);
+	for (size_t i = 0; i < uv.size; i++) {
+		printf("%p\n", uv.u[i]);
+		printf("%s (%d years)\n", uv.u[i]->name, uv.u[i]->age);
+	}
+
+	free_vec(&uv);
 	return 0;
 }
