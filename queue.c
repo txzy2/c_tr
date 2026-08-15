16ad92d2792e99f8b008bfc6b0f62bc227d8364f
txzy2 <txzy2@yandex.ru>
2026-08-15 22:30:47 +0300

    Генерация и добавление в конец

--- queue.c
+++ queue.c
@@ -19,9 +19,36 @@ typedef struct
 	i32 size;
 } Queue;
 
-bool enqueue(Queue *q, struct Node *node) { return true; }
+bool enqueue(Queue *q, const User *user)
+{
+	if (q == NULL || user == NULL)
+	{
+		return false;
+	}
 
-bool dequeue(Queue *q, struct Node *node) { return true; }
+	struct Node *new_node = malloc(sizeof(struct Node));
+	if (new_node == NULL)
+	{
+		return false;
+	}
+	new_node->u = *user;
+	new_node->next = NULL;
+
+	if (q->head == NULL)
+	{
+		q->head = q->tail = new_node;
+	}
+	else
+	{
+		q->tail->next = new_node;
+		q->tail = new_node;
+	}
+
+	q->size++;
+	return true;
+}
+
+bool dequeue(Queue *q, struct Node *node);
 
 void free_q(Queue *queue)
 {
@@ -43,14 +70,53 @@ void free_q(Queue *queue)
 	return;
 }
 
+void print_q(Queue *q)
+{
+	printf("Queue size: %d\nQueue HEAD [%p]\nQueue TAIL [%p]\n", q->size, q->head, q->tail);
+	struct Node *cur = q->head;
+	while (cur != NULL)
+	{
+		printf("   [%p] User id: %d, name: %s, email: %s\n", cur, cur->u.id, cur->u.name, cur->u.email);
+		cur = cur->next;
+	}
+}
+
+bool generate_users(Queue *q)
+{
+	for (i32 i = 0; i < 10; i++)
+	{
+		char name[256], email[256];
+		sprintf(name, "User %d", i + 1);
+		sprintf(email, "user%d@example.com", i + 1);
+
+		User u = {.id = i + 1};
+		snprintf(u.name, sizeof(u.name), "%s", name);
+		u.name[sizeof(u.name) - 1] = '\0';
+		snprintf(u.email, sizeof(u.email), "%s", email);
+		u.email[sizeof(u.email) - 1] = '\0';
+
+		if (!enqueue(q, &u))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
 int main()
 {
 	i8 result = EXIT_SUCCESS;
 
 	Queue q = {.head = NULL, .tail = NULL, .size = 0};
-	printf("HELLO WORLD\n");
 
-	goto cleanup;
+	if (!generate_users(&q))
+	{
+		result = EXIT_FAILURE;
+		goto cleanup;
+	}
+
+	print_q(&q);
 
 cleanup:
 	free_q(&q);
