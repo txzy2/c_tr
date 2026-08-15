eb1d5ed75a24029e9553f82b20e344e753370906
txzy2 <txzy2@yandex.ru>
2026-08-16 01:18:15 +0300

    Каркас

---
+++ bin_tree.c
@@ -0,0 +1,43 @@
+#include "types.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//     50
+//    /  \
+//   30    70
+//  / \    / \
+// 20 40  60  80
+
+struct Node
+{
+	i32 value;
+	struct Node *left;
+	struct Node *right;
+};
+
+typedef struct
+{
+	struct Node *root;
+} Tree;
+
+bool insert(Tree *tree, i32 value);
+
+bool remove_node(Tree *tree, i32 value);
+
+bool search(Tree *tree, i32 value);
+
+void print_tree(Tree *tree);
+
+void free_tree(Tree *tree);
+
+int main()
+{
+	i8 result = EXIT_SUCCESS;
+
+	goto cleanup;
+
+cleanup:
+	return result;
+}
