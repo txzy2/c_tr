fb72bca2b9457d113cd034ce32cc25ed031c5015
txzy2 <txzy2@yandex.ru>
2026-08-15 14:52:51 +0300

    Очистка + доавление

--- stack.c
+++ stack.c
@@ -2,6 +2,7 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
@@ -26,7 +27,26 @@ typedef struct
 User *peek(Stack *stack);
 
 // Add el to top of stack
-bool push(Stack *stack, const User *user);
+bool push(Stack *stack, const User *user)
+{
+	if (stack == NULL || user == NULL)
+	{
+		return false;
+	}
+
+	struct Node *new_node = malloc(sizeof(struct Node));
+	if (new_node == NULL)
+	{
+		return false;
+	}
+
+	new_node->u = *user; // Разыменовыли
+	new_node->next = stack->top;
+	stack->top = new_node;
+	stack->size++;
+
+	return true;
+}
 
 // Delete el top of the stack
 bool pop(Stack *stack);
@@ -34,13 +54,49 @@ bool pop(Stack *stack);
 // Is stack empty?
 bool is_empty(Stack *stack);
 
+// Delete stack
+void free_s(Stack *stack)
+{
+	struct Node *cur = stack->top;
+	while (cur != NULL)
+	{
+		struct Node *next_node = cur->next;
+		free(cur);
+		cur = next_node;
+	}
+	stack->top = NULL;
+	stack->size = 0;
+}
+
+void print_debug(const Stack *stack)
+{
+	printf("top=%p size=%d\n", (void *)stack->top, stack->size);
+	struct Node *cur = stack->top;
+	while (cur != NULL)
+	{
+		printf("  [%p] id=%d name=%s email=%s next=%p\n", (void *)cur, cur->u.id, cur->u.name, cur->u.email,
+		       (void *)cur->next);
+		cur = cur->next;
+	}
+}
+
 int main()
 {
-	i8 result = 0;
+	i8 result = EXIT_SUCCESS;
 
-	printf("HELLO WORLD\n");
-	goto cleanup;
+	User u = {.id = 1, .name = "Anton", .email = "test@test.ru"};
+	Stack s = {.top = NULL, .size = 0};
+
+	if (!push(&s, &u))
+	{
+		fprintf(stderr, "ERROR TO PUSH\n");
+		result = EXIT_FAILURE;
+		goto cleanup;
+	}
+
+	print_debug(&s);
 
 cleanup:
+	free_s(&s);
 	return result;
 }
